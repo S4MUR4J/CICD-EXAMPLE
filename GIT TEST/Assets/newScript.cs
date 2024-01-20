a1ce2805ab59a6c8782b8403d4d27bc10421aea0
S4MUR4J <mucha.maciej2000@gmail.com>
2024-01-20 13:05:06 +0100

    moma

---
+++ GIT TEST/Assets/newScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var name = "string";
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
