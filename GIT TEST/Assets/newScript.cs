af1953be79fb9564fe0aee771efdab4b81cb28f9
S4MUR4J <mucha.maciej2000@gmail.com>
2024-01-20 14:40:51 +0100

    reapir

--- GIT TEST/Assets/newScript.cs
+++ GIT TEST/Assets/newScript.cs
@@ -7,7 +7,7 @@ public class newScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-                                var name = "string";
+        var name = "string";
     }
 
     // Update is called once per frame
